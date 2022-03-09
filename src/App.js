42b4ab419b0f1296e6d97ec3a169359ccd6f738f
Abdulrahmansadek <boody.sad@gmail.com>
2022-03-09 15:22:03 +0100

    add header and create context for currency

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Cryptopage from "./pages/Cryptopage";
+import Header from "./components/Header";
+import { CryptoProvider } from "./context/CryptoContext";
+
+function App() {
+  return (
+    <CryptoProvider>
+      <div className="App">
+        <Header />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/coins/:id" element={<Cryptopage />} />
+          </Routes>
+        </Router>
+      </div>
+    </CryptoProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+import "./Header.css";
+import CryptoContext from "../context/CryptoContext";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Select,
+  MenuItem,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+function Header() {
+  const { currency, setCurrency } = useContext(CryptoContext);
+
+  const handleCurrencyChange = (e) => {
+    setCurrency(e.target.value);
+  };
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position="static" color="transparent">
+        <Container>
+          <Toolbar>
+            <Typography className="title">Crypto Coins</Typography>
+
+            <Select
+              variant="outlined"
+              className="selectItem"
+              value={currency}
+              onChange={handleCurrencyChange}
+            >
+              <MenuItem value={"EUR"}>EUR</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
+
+export default Header;
